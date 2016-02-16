e82d762c0a2e978dda055699cedb2a5e71df064a
Chris Gascoigne <cgascoig@cisco.com>
2016-02-16 14:16:52 +1100

    remove install resources

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,11 +3,6 @@ class profile::base
 {
   include ciscopuppet
   
-  package { 'cisco_node_utils' :
-    provider => 'gem',
-    ensure => present,
-  }
-  
   ntp_server {'ntp.esl.cisco.com':
       ensure => present,
     }

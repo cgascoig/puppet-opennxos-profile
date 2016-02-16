fc142ff4c1089e901f04f026a193db6a35785af6
Chris Gascoigne <cgascoig@cisco.com>
2016-02-16 14:42:40 +1100

    go back to package install

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,12 @@
 class profile::base
 
 {
-  include ciscopuppet
+  # include ciscopuppet
+
+  package { 'cisco_node_utils' :
+    provider => 'gem',
+    ensure => present,
+  }
   
   ntp_server {'ntp.esl.cisco.com':
       ensure => present,
